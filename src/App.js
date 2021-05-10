27a71accf5bc9c377daae7f25e447bec36b69c4a
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-10 14:48:50 -0500

    Display the API response

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-const App = () => <h1>hello</h1>;
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import fetchCategories from './actions';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return <h1>hello!</h1>;
+};
 
 export default App;
