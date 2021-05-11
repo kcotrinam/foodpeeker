27b20cb7d088345c7658b2412e3040ed80e6aab2
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 08:21:26 -0500

    Display Countries

--- src/containers/Countries.jsx
+++ src/containers/Countries.jsx
@@ -1,14 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable implicit-arrow-linebreak */
+import { Flex } from '@chakra-ui/react';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchCountries } from '../actions';
+import Default from '../assets/images/maps/Default.png';
+import Card from '../components/Card';
 
 const Countries = () => {
   const dispatch = useDispatch();
+  const { countries, loading, error } = useSelector((state) => state.countries);
 
   useEffect(() => {
     dispatch(fetchCountries());
-  });
+  }, [dispatch]);
 
-  return <h2>Countries</h2>;
+  const renderCountries = () =>
+    countries.map((country) => (
+      <Card
+        key={country.strArea}
+        name={country.strArea}
+        img={Default}
+        imgAlt="Image of the country showed"
+      />
+    ));
+
+  return (
+    <>
+      <Flex wrap="wrap" display="flex" w="100%">
+        {renderCountries()}
+      </Flex>
+    </>
+  );
 };
 export default Countries;
