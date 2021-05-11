3c256aecbd0aef586a31b14466dba30b8ca0d3be
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 06:55:33 -0500

    Add countryReducer and add the reucer to the combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import categoryReducer from './categoryReducer';
+import countryReducer from './CountryReducer';
 
 const allReducers = combineReducers({
   categories: categoryReducer,
+  countries: countryReducer,
 });
 
 export default allReducers;
