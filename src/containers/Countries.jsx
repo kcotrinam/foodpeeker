267ffd28e29164b6d04e3e792e644ab292b50f52
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 07:06:29 -0500

    Add basic structure for countries component

--- src/containers/Countries.jsx
+++ src/containers/Countries.jsx
@@ -1,3 +1,14 @@
-const Countries = () => <h2>Countries</h2>;
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCountries } from '../actions';
 
+const Countries = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  });
+
+  return <h2>Countries</h2>;
+};
 export default Countries;
