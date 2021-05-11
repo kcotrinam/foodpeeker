2c70c380dcb890464faa0d423bb516fd8929ecd3
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 05:57:26 -0500

    Get state information to render Card component

--- src/containers/Categories.jsx
+++ src/containers/Categories.jsx
@@ -1,18 +1,35 @@
+import { Flex } from '@chakra-ui/react';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import fetchCategories from '../actions';
+import Card from '../components/Card';
 
 const Categories = () => {
   const dispatch = useDispatch();
+  const { categories, loading, error } = useSelector((state) => state.categories);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, [dispatch]);
 
+  const renderCategories = () => {
+    if (loading) return <h1>Loading...</h1>;
+    if (error) return <h1>Error.</h1>;
+
+    return categories.map((category) => (
+      <Card
+        key={category.idCategory}
+        name={category.strCategory}
+        img={category.strCategoryThumb}
+        imgAlt={category.strCategoryDescription}
+      />
+    ));
+  };
+
   return (
-    <div>
-      <h2>Categories</h2>
-    </div>
+    <Flex wrap="wrap" display="flex" w="100%">
+      {renderCategories()}
+    </Flex>
   );
 };
 
