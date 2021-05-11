9aaf25a4c4458c27b34fe07a1f7f6a5616c6b272
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 05:56:58 -0500

    Add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+/* eslint-disable  react/require-default-props */
+import { Box, Image, Text } from '@chakra-ui/react';
+
+const Card = ({ name, img, imgAlt }) => (
+  <>
+    <Box w={{ base: '50%', md: '33.333%' }} position="relative">
+      <Box>
+        <Image src={img} alt={imgAlt} />
+      </Box>
+      <Box minH="50%" position="absolute" bottom="0" right="15%">
+        <Text textAlign="right" fontSize="lg" fontWeight="bold" color="gray.50">
+          {name}
+        </Text>
+      </Box>
+    </Box>
+  </>
+);
+Card.propTypes = {
+  name: PropTypes.string,
+  img: PropTypes.string,
+  imgAlt: PropTypes.string,
+};
+
+export default Card;
