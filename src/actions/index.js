1012f3aac85b59cb5d6a3ed0681d24a466ef1165
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-11 06:55:12 -0500

    Add actionTypes and action fetchCategories

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,12 @@ import {
   FETCH_CATEGORIES_FAILURE,
   FETCH_CATEGORIES_REQUEST,
   FETCH_CATEGORIES_SUCCESS,
+  FETCH_COUNTRIES_FAILURE,
+  FETCH_COUNTRIES_REQUEST,
+  FETCH_COUNTRIES_SUCCESS,
 } from './actionTypes';
 
-const fetchCategories = () => async (dispatch) => {
+export const fetchCategories = () => async (dispatch) => {
   dispatch({ type: FETCH_CATEGORIES_REQUEST });
 
   try {
@@ -17,4 +20,15 @@ const fetchCategories = () => async (dispatch) => {
   }
 };
 
-export default fetchCategories;
+export const fetchCountries = () => async (dispatch) => {
+  dispatch({ type: FETCH_COUNTRIES_REQUEST });
+
+  try {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+    const json = await response.json();
+
+    dispatch({ type: FETCH_COUNTRIES_SUCCESS, payload: json.meals });
+  } catch (error) {
+    dispatch({ type: FETCH_COUNTRIES_FAILURE, payload: error });
+  }
+};
