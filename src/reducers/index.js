e27ff4b309e4c48f5a2ba97f6c1c1fd291af3125
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-12 05:39:06 -0500

    Add reducer to the combine REducers list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import categoryReducer from './categoryReducer';
 import countryReducer from './CountryReducer';
 import mealListReducer from './mealListReducer';
+import recipeReducer from './recipeReducer';
 
 const allReducers = combineReducers({
   categories: categoryReducer,
   countries: countryReducer,
   mealList: mealListReducer,
+  recipe: recipeReducer,
 });
 
 export default allReducers;
