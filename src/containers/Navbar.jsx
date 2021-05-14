79a7b4e62c3ee2180a67b6766c2c896e8d560915
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-14 17:49:46 -0500

    Refactor navbar layout

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,25 +1,34 @@
 /* eslint-disable object-curly-newline */
-import { Box, Grid, Menu, MenuItem } from '@chakra-ui/react';
+import { Box, Center, Flex, Grid, Menu, MenuItem, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <Menu>
-    <Grid templateColumns="repeat(3, 1fr)">
-      <Box bg="tomato">
-        <MenuItem>
-          <Link to="/">Home</Link>
-        </MenuItem>
+    <Grid>
+      <Box bg="red">
+        <Center>
+          <Text fontSize={{ base: '2xl', md: '5xl' }} fontWeight="bold">
+            FOODKEEPER
+          </Text>
+        </Center>
       </Box>
-      <Box bg="tomato">
-        <MenuItem>
-          <Link to="/categories">Categories</Link>
-        </MenuItem>
-      </Box>
-      <Box bg="tomato">
-        <MenuItem>
-          <Link to="/countries">countries</Link>
-        </MenuItem>
-      </Box>
+      <Grid templateColumns="repeat(3, 1fr)">
+        <Flex justifyContent="center">
+          <Link to="/">
+            <MenuItem mx="auto">Home</MenuItem>
+          </Link>
+        </Flex>
+        <Flex justifyContent="center">
+          <Link to="/categories">
+            <MenuItem>Categories</MenuItem>
+          </Link>
+        </Flex>
+        <Flex justifyContent="center">
+          <Link to="/countries">
+            <MenuItem>countries</MenuItem>
+          </Link>
+        </Flex>
+      </Grid>
     </Grid>
   </Menu>
 );
