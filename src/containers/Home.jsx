02247ce4ff2a7bc86505a2fbc02a36ff7f156726
Kevin Cotrina <kcotrinam92@gmail.com>
2021-05-15 07:48:30 -0500

    Add Home page

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,3 +1,22 @@
-const Home = () => <h2>Home</h2>;
+import { Box, Center, Text } from '@chakra-ui/react';
+import bgImage from '../assets/images/food.jpg';
+
+const Home = () => (
+  <>
+    <Box
+      pos="relative"
+      bg={`url(${bgImage})`}
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+    >
+      <Box pos="absolute" w="100%" h="100%" top="0" bg="rgba(0,0,0,0.8)" />
+      <Center h="100%">
+        <Text fontSize="5xl" pos="relative" fontWeight="bold" color="gray.300">
+          Food Peeker
+        </Text>
+      </Center>
+    </Box>
+  </>
+);
 
 export default Home;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,3 +1,4 @@
+import { Grid } from '@chakra-ui/react';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Categories from '../containers/Categories';
@@ -9,15 +10,17 @@ import Recipe from '../containers/Recipe';
 
 const Routes = () => (
   <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/categories" component={Categories} />
-      <Route exact path="/countries" component={Countries} />
-      <Route path="/categories/:name" exact component={MealList} />
-      <Route path="/countries/:name" exact component={MealList} />
-      <Route path="/:id" exact component={Recipe} />
-    </Switch>
+    <Grid minHeight="100vh" templateRows="auto 1fr">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/categories" component={Categories} />
+        <Route exact path="/countries" component={Countries} />
+        <Route path="/categories/:name" exact component={MealList} />
+        <Route path="/countries/:name" exact component={MealList} />
+        <Route path="/:id" exact component={Recipe} />
+      </Switch>
+    </Grid>
   </Router>
 );
 
